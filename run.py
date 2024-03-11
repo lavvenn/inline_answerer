3e17894bb34a5267cf01655688bef4f869056015
lavvenn <zmievsijkonstantin0@gmail.com>
2024-03-11 10:56:15 +0300

    init project

---
+++ run.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from app.handlers import router
+
+from config import TOKEN
+
+async def main():
+    bot = Bot(TOKEN)
+    dp = Dispatcher()
+
+    dp.include_router(router)
+
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
