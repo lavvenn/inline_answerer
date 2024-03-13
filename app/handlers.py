2bb78d40f4d103cddbff6e357e23b28a9eac99ad
lavvenn <zmievsijkonstantin0@gmail.com>
2024-03-13 19:03:01 +0300

    add command and all message handlers

--- app/handlers.py
+++ app/handlers.py
@@ -1,3 +1,14 @@
-from aiogram import Router
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.filters import CommandStart
 
 router = Router()
+
+@router.message(F.text == '/start')
+async def command_start(message: Message):
+    await message.answer(text='hello')
+
+@router.message()
+async def all_message(message: Message):
+    answer = 'wow'
+    await message.answer(text=answer)
