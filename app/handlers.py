f576e6a4f2c389bf31c0c55fd867aff5fad1af58
lavvenn <zmievsijkonstantin0@gmail.com>
2024-03-14 11:27:43 +0300

    add GPT3.5 response

--- app/handlers.py
+++ app/handlers.py
@@ -1,8 +1,19 @@
+import os
+
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 
+from g4f.client import Client
+import g4f
+
 router = Router()
+
+client = Client()
+
+
+
+
 
 @router.message(F.text == '/start')
 async def command_start(message: Message):
@@ -10,5 +21,10 @@ async def command_start(message: Message):
 
 @router.message()
 async def all_message(message: Message):
-    answer = 'wow'
-    await message.answer(text=answer)
+    
+    response = await g4f.ChatCompletion.create_async(
+    model=g4f.models.gpt_35_turbo_16k,
+    messages=[{"role": "user", "content": message.text}],
+    )
+
+    await message.answer(text= response)
